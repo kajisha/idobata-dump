0924c76e797438c683b6b1b8e009cb2a5d9de10b
kajisha <kajisha@gmail.com>
2014-08-08 13:14:05 +0900

    oops

--- src/js/dump.js
+++ src/js/dump.js
@@ -5,7 +5,7 @@ var addDumpButton = function(){
       var contents = '<div id="dump_contents" class="modal fade" aria-hidden="true" style="display: none;"><div class="modal-header"><button class="close" data-dismiss="modal" type="button">×</button><h3>pick dump</h3></div><div class="modal-body">';
       contents += '<img class="dump" src="https://avatars2.githubusercontent.com/u/1160522?v=2&s=400#.png" title="dany1468 the developer">';
       contents += '<img class="dump" src="https://idobata.s3.amazonaws.com/uploads/attachment/image/6406/967d853a-2b9c-4e1f-95b9-749ddc078092/ldtm.png" title="LDTM">';
-      contents += '<img class="dump" src="https://idobata.s3.amazonaws.com/uploads/attachment/image/6408/eff2668e-93d4-4872-ae00-f887a10976a5/turban.png" title="Nice Tarban">';
+      contents += '<img class="dump" src="https://idobata.s3.amazonaws.com/uploads/attachment/image/6413/ea2f0578-73af-4613-9633-024b99e388ed/turban.jpg" title="Nice Tarban">';
       contents += '</div></div>';
       $('body').append(contents);
     }
