f92cf92b72eccfeec951622dfe4d9d31e7d6e6c9
kajisha <kajisha@gmail.com>
2014-08-08 13:08:16 +0900

    add stamps

--- src/js/dump.js
+++ src/js/dump.js
@@ -1,11 +1,13 @@
 var addDumpButton = function(){
   setTimeout(function(){
     if ($('#dump').length == 0){
-      $('.message-form textarea').before('<button id="dump" class="btn attach-file-text"><img src="'+chrome.extension.getURL('images/btn-dump.png')+'"></button>')
-      var contents = '<div id="dump_contents" class="modal fade" aria-hidden="true" style="display: none;"><div class="modal-header"><button class="close" data-dismiss="modal" type="button">×</button><h3>pick dump</h3></div><div class="modal-body">'
-      contents += '<img class="dump" src="https://avatars2.githubusercontent.com/u/1160522?v=2&s=400#.png">'
-      contents += '</div></div>'
-      $('body').append(contents)
+      $('.message-form textarea').before('<button id="dump" class="btn attach-file-text"><img src="' + chrome.extension.getURL('images/btn-dump.png')+'"></button>');
+      var contents = '<div id="dump_contents" class="modal fade" aria-hidden="true" style="display: none;"><div class="modal-header"><button class="close" data-dismiss="modal" type="button">×</button><h3>pick dump</h3></div><div class="modal-body">';
+      contents += '<img class="dump" src="https://avatars2.githubusercontent.com/u/1160522?v=2&s=400#.png" title="dany1468 the developer">';
+      contents += '<img class="dump" src="https://idobata.s3.amazonaws.com/uploads/attachment/image/6406/967d853a-2b9c-4e1f-95b9-749ddc078092/ldtm.png" title="LDTM">';
+      contents += '<img class="dump" src="https://idobata.s3.amazonaws.com/uploads/attachment/image/6408/eff2668e-93d4-4872-ae00-f887a10976a5/turban.png" title="Nice Tarban">';
+      contents += '</div></div>';
+      $('body').append(contents);
     }
   }, 500)
 }
